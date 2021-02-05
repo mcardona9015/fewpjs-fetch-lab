5c5cd6df2ea125c8b6533081fb7683b443060571
mcardona9015 <mcardona9015@gmail.com>
2021-02-04 21:20:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function fetchBooks() {
+  
+  function fetchBooks() {
+    fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => {renderBooks(json)});
+  }
 
-}
-
+let booksArray
 function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
@@ -9,8 +13,18 @@ function renderBooks(books) {
     h2.innerHTML = book.name
     main.appendChild(h2)
   })
+
+  booksArray = books
 }
+// renderBooks(books)
 
 document.addEventListener('DOMContentLoaded', function() {
+  
+
   fetchBooks()
+  setTimeout(() => {console.log(booksArray)}, 1000)
+
+  
 })
+
+
